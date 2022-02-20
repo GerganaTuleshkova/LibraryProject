ad19fbdce8ea0f3f34fa64e9a9f5e05d9600cf7b
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-02-20 11:17:42 +0200

    key removed

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6vo!ndqr1j((#j^fb#vyi#v_sfmjw(mk=a((7rr#483op6f+5%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
